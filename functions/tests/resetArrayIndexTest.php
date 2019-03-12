bbc8fdbd38d43e821090bf8d1e0c594e3d8a7598
admin <admin>
2019-03-11 20:47:02 -0400

    New test case

--- functions/tests/resetArrayIndexTest.php
+++ functions/tests/resetArrayIndexTest.php
@@ -41,4 +41,9 @@ final class resetArrayIndexTest extends TestCase
 
         $this->assertEquals(resetArrayIndex($inputArr), $expectedArr);
     }
+
+    public function testHandlesEmptyArraysGracefully() : void
+    {
+        $this->assertEquals(resetArrayIndex([]), []);
+    }
 }
