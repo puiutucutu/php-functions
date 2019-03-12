a883deaff40941ec83a436f526c557df7eb0bfd3
admin <admin>
2019-03-11 20:09:41 -0400

    Renamed function to make more sense

---
+++ functions/hasKey.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @param array  $arr
+ * @param string $key
+*
+* @return bool
+    */
+function hasKey(array $arr, $key)
+{
+    return (
+        isset($arr[$key]) or
+        array_key_exists($key, $arr)
+    );
+}
