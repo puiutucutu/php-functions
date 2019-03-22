79a4b62ccff67164ebac4133614c7f6dbd3ea76e
admin <admin>
2019-03-22 17:39:41 -0400

    Improved fn

--- functions/reindexUsingCallbackKey.php
+++ functions/reindexUsingCallbackKey.php
@@ -2,16 +2,30 @@
 
 /**
  * @param array    $items
- * @param callable $callback
+ * @param callable $callback A function which will be provided the current
+ *                           iterated value as an argument and is expected to
+ *                           return a string representing a key value.
  *
  * @return array
  */
 function reindexUsingCallbackKey(array $items, $callback)
 {
     $temp = [];
-    foreach ($items as $value) {
-        $id = $callback($value);
-        $temp[$id] = $value;
+    foreach ($items as $k => $value) {
+        $keyName = $callback($value);
+        if (!!$keyName) {
+            throw new RuntimeException(
+                "The returned index key from the callback function is empty"
+            );
+        }
+
+        if (!isset($value[$keyName])) {
+            throw new OutOfBoundsException(
+                "Key name `${keyName}` does not exist on item at index `${k}`"
+            );
+        }
+
+        $temp[$keyName] = $value;
     }
 
     return $temp;
