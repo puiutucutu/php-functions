ae14ca1c90be1f8773699a3c23d90d4b5719651b
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-08 11:27:27 -0400

    Create surroundWithBracketsClass.php

---
+++ functions/strings/surroundWithBracketsClass.php
@@ -0,0 +1,67 @@
+<?php
+
+abstract class Bracket
+{
+    private $openingBrace = "";
+
+    private $closingBrace = "";
+
+    protected function __construct($openingBrace, $closingBrace)
+    {
+        $this->openingBrace = $openingBrace;
+        $this->closingBrace = $closingBrace;
+    }
+
+    public function getOpeningBrace()
+    {
+        return $this->openingBrace;
+    }
+
+    public function getClosingBrace()
+    {
+        return $this->closingBrace;
+    }
+}
+
+final class ParenthesesBracket extends Bracket
+{
+    public function __construct()
+    {
+        parent::__construct("(", ")");
+    }
+}
+
+final class BracesBracket extends Bracket
+{
+    public function __construct()
+    {
+        parent::__construct("{", "}");
+    }
+}
+
+final class SquareBrackets extends Bracket
+{
+    public function __construct()
+    {
+        parent::__construct("[", "]");
+    }
+}
+
+/**
+ * @param \Bracket $bracketType
+ *
+ * @return \Closure Expects a string to surround with requested bracket type.
+ */
+function surroundWithBrackets(Bracket $bracketType)
+{
+    return function($x) use ($bracketType) {
+        $start = $bracketType->getOpeningBrace();
+        $end = $bracketType->getClosingBrace();
+        return "${start}${x}${end}";
+    };
+}
+
+$myStr = "abc";
+$myStrBracketed = surroundWithBrackets (new BracesBracket()) ($myStr);
+
+var_dump($myStrBracketed);
