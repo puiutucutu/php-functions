204a65f15a52539d1e832c1a9b57627b8caf680a
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-06-16 21:16:05 -0400

    Updated test require

--- tests/array/chunkTest.php
+++ tests/array/chunkTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "array/chunk.php";
+require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "src/array/chunk.php";
 
 final class chunkTest extends TestCase
 {
