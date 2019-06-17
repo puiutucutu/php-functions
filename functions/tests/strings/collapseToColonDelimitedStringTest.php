7693d37febd77c24d766a3bafc388d9b33649c92
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-06-16 20:42:11 -0400

    Updated require pathing

--- functions/tests/strings/collapseToColonDelimitedStringTest.php
+++ functions/tests/strings/collapseToColonDelimitedStringTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-require_once dirname(__DIR__, 2)
+require_once dirname(dirname(__DIR__))
     . DIRECTORY_SEPARATOR
     . "strings"
     . DIRECTORY_SEPARATOR
