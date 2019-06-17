d7759d5d9af3bc1dcd60923b5593e3eefe8738b8
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-06-17 18:18:12 -0400

    Added new fn

---
+++ src/array/pick.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @param string[]|int[] $keys
+ *
+ * @return Closure A function that takes an associative array as the only
+ * argument and returns an array of values matched for each of the supplied
+ * keys.
+ */
+function pick($keys)
+{
+    return function ($obj) use ($keys) {
+        $output = [];
+        $objKeys = array_keys($obj);
+
+        foreach ($keys as $key) {
+            if (in_array($key, $objKeys)) {
+                $output[$key] = $obj[$key];
+            }
+        }
+
+        return $output;
+    };
+}
---
+++ tests/array/pickTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "src/array/pick.php";
+
+final class pickTest extends TestCase
+{
+    public function testReturnsExpectedValueWhenArrayIsEmpty() : void
+    {
+        $this->assertEquals(pick ([]) ([]), []);
+    }
+
+    public function testReturnsExpectedValue() : void
+    {
+        $data = [
+            "id" => 123,
+            "brand" => "bmw",
+            "model" => "7 Series",
+        ];
+
+        $this->assertEquals(
+            pick(["id", "brand"]) ($data),
+            ["id" => 123, "brand" => "bmw"]
+        );
+    }
+}
