4410e9e31dd68db8ca0d4037bdb8b8c1cc629529
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-06-16 21:00:46 -0400

    New fn

---
+++ functions/tests/array/reduceNativeTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "array/reduceNative.php";
+
+final class reduceNativeTest extends TestCase
+{
+    public function testReturnsExpectedValue() : void
+    {
+        $reduceable = [1, 2, 3, 4];
+        $reducer = function ($sum, $toAdd) {
+            return ($sum + 1) * $toAdd;
+        };
+
+        $numsAddedByReducing = reduce($reduceable, $reducer, 0);
+
+        $this->assertEquals(
+            $numsAddedByReducing,
+            64
+        );
+    }
+}
