fde06e355668032250ec38a326bc34093bdf09d0
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-06-19 16:33:36 -0400

    Update createBoundPlaceholders.php

--- src/helpers/sql/createBoundPlaceholders.php
+++ src/helpers/sql/createBoundPlaceholders.php
@@ -2,11 +2,27 @@
 
 /**
  * Create a string for parameter placeholders filled up to the # of params,
- * note that there needs to be one less comma than the count of ids.
+ * note that there needs to be one less comma than the count of items.
  *
- * @param array $data
+ * @param array $xs
  *
  * @return string Given an array of three items, returns `(?),(?),(?)`
+ * @example
+ *
+ *   $boundPlaceholders = createBoundPlaceholders($names);
+ *
+ *   $sql = <<<SQL
+ *
+ *   SELECT *
+ *   FROM addresses
+ *   WHERE city = ? AND companyName IN ($boundPlaceholders)
+ *
+ *   SQL;
+ *
+ *   $stmt = $this->dbh->prepare($sql);
+ *   $stmt->execute(array_merge(["New York"], $names));
+ *   $rows = $stmt->fetchAll();
+ *
  */
 function createBoundPlaceholders(array $xs)
 {
