b2493c9d3950fea3846b50f426b23b9e85715634
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-07-17 12:09:36 -0400

    Create arrayToCsv.php

---
+++ src/other/arrayToCsv.php
@@ -0,0 +1,12 @@
+/**
+ * @param string $fileName
+ * @param array  $dataset
+ */
+function arrayToCsv($fileName, array $dataset)
+{
+    $fp = fopen("{$fileName}.csv", "wb");
+    foreach ($dataset as $line) {
+        fputcsv($fp, $line);
+    }
+    fclose($fp);
+}
