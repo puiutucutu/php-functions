e66a42e2abae7f864f8293ac005f8fca8c177db4
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-07-17 12:18:42 -0400

    Create insertMultiple.php

---
+++ src/helpers/sql/insertMultiple.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @param \PDO   $dbh
+ * @param string $partialInsertSql
+ * @param array  $data A 2-d array of data.
+ *
+ * @return void
+ * @see https://docs.microsoft.com/en-us/sql/t-sql/statements/insert-transact-sql?view=sql-server-2017
+ * @see https://docs.microsoft.com/en-us/sql/t-sql/queries/table-value-constructor-transact-sql?view=sql-server-2017
+ */
+function insertMultiple(PDO $dbh, $partialInsertSql, array $data)
+{
+    $wrapWithParentheses = function($x) {
+        return "({$x})";
+    };
+
+    $columnListPlaceholder = array_fill(0, count($data[0]), "?");
+    $columnListPlaceholderString = $wrapWithParentheses(implode(",", $columnListPlaceholder));
+
+    $params = [];
+    $valuesListPlaceholders = [];
+    foreach ($data as $datum) {
+        $valuesListPlaceholders[] = $columnListPlaceholderString;
+        foreach ($datum as $k => $v) {
+            $params[] = $v;
+        }
+    }
+
+    $valuesListPlaceholdersStringified = implode("," . PHP_EOL, $valuesListPlaceholders);
+    $stmt = $dbh->prepare("{$partialInsertSql} VALUES {$valuesListPlaceholdersStringified}");
+    $stmt->execute($params);
+}
